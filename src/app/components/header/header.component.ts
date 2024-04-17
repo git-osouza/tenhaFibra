b2cf18680354bffc7ffda6f0935acf8ac038ce3d
Ederson Guilherme de Souza <70489050+git-osouza@users.noreply.github.com>
2024-04-17 09:35:06 -0300

    adjust button whatsapp

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { PhoneFormatPipe } from '../../shared/pipes/phone-format.pipe';
 import { environment } from './../../../environments/environment';
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [PhoneFormatPipe],
+  imports: [PhoneFormatPipe, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, AfterViewInit {
   apiWhatsapp: string = `${environment.apiWhatsapp}${environment.whatsappNumber}`;
   whatsappNumber: string = environment.whatsappNumber;
+  isMobileMenuOpen: boolean = false;
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.cdr.detectChanges();
+  }
+
+  ngAfterViewInit(): void {
+    this.cdr.detectChanges();
+  }
+
+  toggleMobileMenu() {
+    console.log('aqui');
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+
 }
