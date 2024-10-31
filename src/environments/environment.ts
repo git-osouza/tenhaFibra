57a019611977bc4630150d1e735f292dd356cf21
Ederson Guilherme de Souza <70489050+git-osouza@users.noreply.github.com>
2024-10-31 10:58:27 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
+
+const hostApi =  'https://api.tenhafibra.com.br/tenha-fibra/php/';
+
 export const environment = {
     production: false,
     whatsappNumber: "08001507777",
     apiWhatsapp: `https://api.whatsapp.com/send?phone=5551`,
     apiVitrini: 'https://api-assinefibra.automatuslab.com/ConsultaVitrine',
-    apiLead: 'https://api.tenhafibra.com.br/tenha-fibra/php/conect.php',
-    apiAuth: 'https://api.tenhafibra.com.br/tenha-fibra/php/auth.php',
-    apiGetLeads: 'https://api.tenhafibra.com.br/tenha-fibra/php/leads.php'
+    apiLead: `${hostApi}conect.php`,
+    apiAuth: `${hostApi}auth.php`,
+    apiGetLeads: `${hostApi}leads.php`
 };
